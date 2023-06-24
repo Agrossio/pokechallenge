c99df79249d73bdfb0ca38d743fe171f3ab0d100
Agrossio <abossio@gmail.com>
2023-06-24 15:07:56 -0300

    initial-setup

---
+++ src/test/java/com/jemersoft/pokechallenge/PokechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jemersoft.pokechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
