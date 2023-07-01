b99b3902a5568d987bbaaf9348a87b5d7bc79f12
Agrossio <abossio@gmail.com>
2023-07-01 17:16:59 -0300

    swagger-ui-https-ok

--- src/main/java/com/jemersoft/pokechallenge/config/SpringFoxSwaggerConfig.java
+++ src/main/java/com/jemersoft/pokechallenge/config/SpringFoxSwaggerConfig.java
@@ -1,9 +1,14 @@
 package com.jemersoft.pokechallenge.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +16,12 @@ import org.springframework.context.annotation.Configuration;
 public class SpringFoxSwaggerConfig {
 
 //    https://springdoc.org/#migrating-from-springfox
+//    swagger con https: https://github.com/springdoc/springdoc-openapi/issues/118
+@Value("${server.servlet.context-path}") String contextPath;
     @Bean
     public OpenAPI api(){
         return new OpenAPI()
+//                .addServersItem(new Server().url(contextPath))
                 .info(new Info()
                         .title("PokeChallenge Springboot API")
                         .version("V1")
